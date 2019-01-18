09c798fba858bdc861a4398ab90db06b76a97d1e
bauw <bauw@crans.org>
2019-01-18 18:31:41 +0100

    hum

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 """
 Contains the definition of the agent that will run in an
@@ -38,14 +39,13 @@ class QLearningAgent:
         # https://github.com/vmayoral/basic_reinforcement_learning/blob/master/tutorial4/README.md
         # https://stats.stackexchange.com/questions/184657/what-is-the-difference-between-off-policy-and-on-policy-learning
         """
-        self.maxIteration = 1000
-        self.gamma = 0.9 # discount rate parameter
-        self.alpha = 0.5 # step-size parameter
-        self.nbrActSamples = 100 # how many to sample at each time step
+        self.gamma = 0.75 # discount rate parameter
+        self.alpha = 0.4 # step-size parameter
+        self.nbrActSamples = 200 # how many to sample at each time step
         self.nextActions = np.random.uniform(-2,2,self.nbrActSamples) # re-sampled each call
         self.Q = 0
         self.difference = 0
-        self.weights = np.ones((4,)) # shape of range observation + action tuple
+        self.weights = np.ones((5,)) # shape of range observation + action tuple
         self.epsilon = 0.1 # epsilon for epsilon greedy policy
 
     def act(self, observation):
@@ -54,8 +54,12 @@ class QLearningAgent:
         - epsilon greedy policy to select next action (choose with best Q)
         """
         greedySample = np.random.uniform(0,1)
+
+        # exploration
         if greedySample < self.epsilon:
-            return([np.random.uniform(-2,2)]) # exploration
+            return([np.random.uniform(-2,2)])
+
+        # exploitation
         else:
         # return best action according to Q(s,a) values
         # to estimate Q(s,a) use our estimator (weighted sum of features)
@@ -74,8 +78,7 @@ class QLearningAgent:
             # re-sample next actions for next function call
             self.nextActions = np.random.uniform(-2,2,self.nbrActSamples)
             bestActionArray = [bestAction]
-            return(bestActionArray)  # sometimes array, sometimes not, WTF
-            # return(bestAction)
+            return(bestActionArray)
 
     def reward(self, observation, action, reward):
         """
@@ -86,11 +89,11 @@ class QLearningAgent:
 
         - features are 3 elements of observation tuple + action value
         """
-        # action = action[0]
         # print("action in reward:",action)
         greedyQ = self.predict(observation,action)
         features = self.computeFeatures(observation,action)
         self.difference = (reward+self.gamma*greedyQ)-self.Q
+        print(self.difference)
 
         # we need "exact Q" below to update our weights
         self.Q += self.alpha*self.difference
@@ -98,7 +101,10 @@ class QLearningAgent:
         newQ = 0
         for wIndex in range(self.weights.shape[0]):
             self.weights[wIndex]+=self.alpha*self.difference*features[wIndex]
+            # print("weights:",self.weights)
+            # print("features:",features)
             newQ += self.weights[wIndex]*features[wIndex]
+
         self.Q = newQ
 
     def predict(self,observation,action):
@@ -110,25 +116,48 @@ class QLearningAgent:
         # print("estimQ:",estimQ)
         return(estimQ)
 
+    # def computeFeatures(self,observation,action):
+    #     """
+    #     FEATURES BASED ON OBSERVATIONS AND ACTION
+    #
+    #     - return array of features
+    #     - this array should have the same size as the array of weights
+    #     """
+    #     # lets try features = observation and action
+    #     featuresSize = len(observation) + 1 # 1 is so-called len(action)
+    #     features = np.zeros((featuresSize,))
+    #     for featureIndex in range(featuresSize):
+    #         if (featureIndex < featuresSize-1):
+    #             features[featureIndex] = observation[featureIndex]**2
+    #         if (featureIndex == featuresSize-1):
+    #             # sometimes array, sometimes not, WTF
+    #             if isinstance(action,(list,)):
+    #                 features[featureIndex] = action[0]
+    #             #print("Action for features:",action)
+    #             else:
+    #                 features[featureIndex] = action
+    #     # print("features returned:",features)
+    #     return(features)
+
     def computeFeatures(self,observation,action):
         """
+        FEATURES WITH ONLY OBSERVATIONS
+
         - return array of features
         - this array should have the same size as the array of weights
         """
         # lets try features = observation and action
-        featuresSize = len(observation) + 1 # 1 is so-called len(action)
+        featuresSize = self.weights.shape[0]
         features = np.zeros((featuresSize,))
-        for featureIndex in range(featuresSize):
-            if (featureIndex < featuresSize-1):
-                features[featureIndex] = observation[featureIndex]
-            if (featureIndex == featuresSize-1):
-                # sometimes array, sometimes not, WTF
-                if isinstance(action,(list,)):
-                    features[featureIndex] = action[0]
-                #print("Action for features:",action)
-                else:
-                    features[featureIndex] = action
-        # print("features returned:",features)
+        for featureIndex in range(3): # 3 first features are observation first
+            features[featureIndex] = observation[featureIndex]
+        features[0] = features[0]**2
+        features[1] = features[1]**2
+        features[2] = np.around(features[2],decimals=2)
+        # features[3] = features[0]*features[1]
+        features[3] = np.around(math.asin(features[1]),decimals=2)**2
+        features[4] = np.around(math.acos(features[0]),decimals=2)**2
         return(features)
 
 Agent = QLearningAgent
+# Agent = RandomAgent
